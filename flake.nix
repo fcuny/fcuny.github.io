f0ca9c420698df2fccf4dca156c5c6837fe61b40
Franck Cuny <franck@fcuny.net>
2022-05-10 19:18:21 -0700

    another try

--- flake.nix
+++ flake.nix
@@ -10,11 +10,32 @@
     flake-utils.lib.eachDefaultSystem (system:
       let pkgs = nixpkgs.legacyPackages.${system};
       in {
+        defaultPackage = with pkgs;
+          stdenv.mkDerivation {
+            pname = "fcuny.net";
+            version = self.lastModifiedDate;
+            src = ./.;
+            buildInputs = [ hugo git ];
+            buildPhase = ''
+              mkdir -p $out
+              hugo --minify --destination $out
+            '';
+            dontInstall = true;
+          };
         defaultApp = pkgs.writers.writeBashBin "run-hugo" ''
           set -e
           set -o pipefail
           export PATH=${pkgs.lib.makeBinPath [ pkgs.hugo pkgs.git ]}
           hugo server -D
+        '';
+
+        deploy = pkgs.writers.writeBashBin "run-deploy" ''
+          set -e
+          set -o pipefile
+          export PATH=${
+            pkgs.lib.makeBinPath [ pkgs.hugo pkgs.git pkgs.jq pkgs.flyctl ]
+          }
+          ./scripts/deploy.sh
         '';
 
         devShell =
