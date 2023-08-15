b310c113f76fd415ffbbbef8c51914e94c915797
Franck Cuny <franck@fcuny.net>
2023-08-14 19:21:30 -0700

    remove unused configurations
    Remove the configuration for fly.io and the configuration for Caddy
    since I'm running this on GitHub pages now.
    
    Remove unused targets from the flake configuration and drop the pandoc
    dependency. The resume is just a web page accessible at [1].
    
    [1] https://fcuny.net/resume.html

--- flake.nix
+++ flake.nix
@@ -10,7 +10,6 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-        caddyfile = ./Caddyfile;
       in
       {
         packages = {
@@ -19,34 +18,13 @@
               pname = "fcuny.net";
               version = self.lastModifiedDate;
               src = ./.;
-              buildInputs = [ hugo git pandoc texlive.combined.scheme-tetex ];
+              buildInputs = [ hugo git ];
               buildPhase = ''
                 mkdir -p $out
                 ${pkgs.hugo}/bin/hugo --minify --destination $out
-                ${pkgs.pandoc}/bin/pandoc --self-contained --css static/css/resume.css \
-                  --from org --to html --output $out/resume.html content/resume.org
-                ${pkgs.pandoc}/bin/pandoc --self-contained --css static/css/resume.css \
-                  --from org --to pdf --output $out/resume.pdf content/resume.org
               '';
               dontInstall = true;
             };
-          container = pkgs.dockerTools.buildLayeredImage {
-            name = self.packages."${system}".site.pname;
-            tag = self.packages."${system}".site.version;
-            config = {
-              Cmd = [ "${pkgs.caddy}/bin/caddy" "run" "--adapter" "caddyfile" "--config" "${caddyfile}" ];
-              Env = [
-                "SITE_ROOT=${self.packages."${system}".site}"
-              ];
-            };
-          };
-          deploy = pkgs.writeShellScriptBin "deploy" ''
-            set -euxo pipefail
-            export PATH="${pkgs.lib.makeBinPath [(pkgs.docker.override { clientOnly = true; }) pkgs.flyctl]}:$PATH"
-            archive=${self.packages.x86_64-linux.container}
-            image=$(docker load < $archive | tail -n1 | ${pkgs.gawk}/bin/awk '{ print $3; }')
-            flyctl deploy --image $image --local-only
-          '';
           hugo = pkgs.writeShellScriptBin "hugo" ''
             set -euo pipefail
             export PATH=${pkgs.lib.makeBinPath [ pkgs.hugo pkgs.git ]}
